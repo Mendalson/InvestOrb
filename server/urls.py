14d8a91da7b61e620d123c312cb0f5159f300f9f
Grisha <grgrlbdv@icloud.com>
2025-08-26 01:11:01 +0300

    Create 'users' directory

--- server/urls.py
+++ server/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -24,6 +25,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page, name='home_page'),
+    path('user/', include('user.urls'), name='user'),
 ]
 
 
