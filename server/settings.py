bd99db2d4f4d9050062c9fd541c7c59b4019408c
Grisha <grgrlbdv@icloud.com>
2025-08-28 23:19:06 +0300

    Remade model 'User'

--- server/settings.py
+++ server/settings.py
@@ -132,4 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = 'users.User'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
-class User(models.Model):
+class User(AbstractUser):
     username = models.CharField(max_length=100, unique=True, null=False, blank=False)
-    password = models.CharField(max_length=50, null=False, blank=False)
+    password = models.CharField(max_length=128, null=False, blank=False)
 
     def __str__(self):
         return self.username
