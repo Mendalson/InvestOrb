b39c9e02cc72b6391b3522d06261f63c6c40006e
Grisha <grgrlbdv@icloud.com>
2025-09-01 17:23:22 +0300

    Changed the routing

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.main_view, name='main'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 ]
