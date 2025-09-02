a90685cfdd7d1b1a55563ed7556f50d8e8e3109a
Grisha <grgrlbdv@icloud.com>
2025-09-02 13:06:43 +0300

    Add auth redirect for unregistered users

--- analytics/views.py
+++ analytics/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def main_view(request):
+    if not request.user.is_authenticated:
+        return redirect('users:login')
     return render(request, 'main_page.html')
