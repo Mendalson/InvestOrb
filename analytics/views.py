ecf90ef8ebc5f51e23ab81e1f2a4036d538aa93a
Mendalson <grgrlbdv@icloud.com>
2025-10-03 23:24:25 +0300

    feat: create prototype page for sending reports

--- analytics/views.py
+++ analytics/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect
+from .parsing import parsing_sber_document
 
 
-# Create your views here.
 def main_view(request):
     if not request.user.is_authenticated:
         return redirect('users:login')
     return render(request, 'main_page.html')
+
+
+def add_document_view(request):
+    if not request.user.is_authenticated:
+        return redirect('users:login')
+    if request.method == 'POST':
+        parsing_sber_document(request.files)
+        return redirect('analytics:main')
+    return render(request, 'add_document.html')
