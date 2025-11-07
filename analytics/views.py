056b1e72b03c14a1a35c70f56895d7c161ae6b93
Mendalson <grgrlbdv@icloud.com>
2025-11-07 11:55:27 +0300

    feat: created a view for processing reports

--- analytics/views.py
+++ analytics/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from .parsing import parsing_sber_document
+from pathlib import Path
+from .parsing import parse_sber_html
+from . import forms
 
 
 def main_view(request):
@@ -11,7 +13,27 @@ def main_view(request):
 def add_document_view(request):
     if not request.user.is_authenticated:
         return redirect('users:login')
+
     if request.method == 'POST':
-        parsing_sber_document(request.files)
-        return redirect('analytics:main')
-    return render(request, 'add_document.html')
+        form = forms.UserReport(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded_files = request.FILES.getlist('file')
+
+            if not uploaded_files:
+                return render(request, 'add_document.html', {'form': form})
+
+            for uploaded_file in uploaded_files:
+                fn = Path(uploaded_file.name)
+
+                if fn.suffix.lower() != '.html':
+                    continue
+
+                parse_sber_html(uploaded_file, request.user)
+
+            return redirect('analytics:main')
+        else:
+            print("Форма невалидна:", form.errors)
+    else:
+        form = forms.UserReport()
+
+    return render(request, 'add_document.html', {'form': form})
